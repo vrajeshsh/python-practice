3ae37d2a164feb4d6d32375a0be97fac181c48a5
Vrajesh <vrajeshshah13@gmail.com>
2022-06-24 18:13:05 -0400

    Update 07. First and Last Word Switch.py

--- 06. Strings/02. Word Applns/07. First and Last Word Switch.py
+++ 06. Strings/02. Word Applns/07. First and Last Word Switch.py
@@ -4,6 +4,9 @@ os.system("cls")
 print("To switch first and last words in a sentence:")
 sent = input("Enter the sentence:\n").strip()
 
-new_sent = sent[sent.rfind(" ")+1:]+sent[sent.find(" "):sent.rfind(" ")+1]+sent[:sent.find(" ")]
+
+end = "" if sent[-1].isalnum() else sent[-1]
+last = sent[sent.rfind(" ")+1:] if sent[-1].isalnum() else sent[sent.rfind(" ")+1:-1]
+new_sent = last +sent[sent.find(" "):sent.rfind(" ")+1]+sent[:sent.find(" ")]+end
 
 print("\nSentence after switch:\n",new_sent,"\n",sep='')
